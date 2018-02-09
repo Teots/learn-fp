0e83b9016f66ed6dfc5623db2826e4515af1d879
Christian Stein <christian.stein@xing.com>
2018-02-09 16:11:25 +0100

    Monads

--- src/main/scala/learnfp/monad/State.scala
+++ src/main/scala/learnfp/monad/State.scala
@@ -5,8 +5,13 @@ import learnfp.functor.StateInstance._
 
 object StateInstance {
   implicit def stateMonadInstance[S] = new Monad[({type E[X] = State[S, X]})#E]() {
-    override def pure[A](a: A): State[S, A] = ???
-    override def flatMap[A, B](a: State[S, A])(fx: A => State[S, B]): State[S, B] = ???
+    override def pure[A](a: A): State[S, A] = State(state => (state, a))
+    override def flatMap[A, B](a: State[S, A])(fx: A => State[S, B]): State[S, B] = {
+      State { state =>
+        val (newState, out) = a.run(state)
+        fx(out).run(newState)
+      }
+    }
   }
 
   implicit def stateToMonadOps[S, A](a:State[S, A]) = new MonadOps[A, ({type E[X] = State[S, X]})#E](a)
